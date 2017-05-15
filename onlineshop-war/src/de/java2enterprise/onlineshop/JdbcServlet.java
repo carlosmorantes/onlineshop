70bf991ee0047068ae33b70f103a8af46e40aaff
carrcarl <carrcarl@DEL5484.ppal.directory>
2017-05-15 10:40:37 +0200

    set up init parameters in a file properties

--- onlineshop-war/src/de/java2enterprise/onlineshop/JdbcServlet.java
+++ onlineshop-war/src/de/java2enterprise/onlineshop/JdbcServlet.java
@@ -1,6 +1,9 @@
 package de.java2enterprise.onlineshop;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Properties;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,25 +11,24 @@ import javax.servlet.http.HttpServletResponse;
 
 public class JdbcServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public void doGet(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
-		final String driver = getInitParameter("driver");
-		final String url = getInitParameter("url");
-		final String username =	getInitParameter("username");
-		final String password =	getInitParameter("password");
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		final String jdbc_properties = getInitParameter("jdbc_properties");
+		final ServletContext application = getServletContext();
+		
+		final InputStream in = application.getResourceAsStream(jdbc_properties);
+		final Properties p = new Properties();
+		
+		p.load(in);
 		response.setContentType("text/html;charset=UTF-8");
 		
-		PrintWriter out = response.getWriter();
+		final PrintWriter out =	response.getWriter();
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<body>");
-		out.println(
-				"<br>driver: " + driver);
-		out.println(
-				"<br>url: " + url);
-		out.println(
-				"<br>username: " + username);
-		out.println(
-				"<br>password: " + password);
+		out.println("<br>driver: " + p.getProperty("driver"));
+		out.println("<br>url: " + p.getProperty("url"));
+		out.println("<br>username: " + p.getProperty("username"));
+		out.println("<br>password: " + p.getProperty("password"));
 		out.println("</body>");
 		out.println("</html>");
 	}
