43b34e031dd0ebbc41959a2c7edb0d30b184468e
carrcarl <carrcarl@DEL5484.ppal.directory>
2017-05-16 10:10:44 +0200

    read data

--- onlineshop-war/src/de/java2enterprise/onlineshop/SellServlet.java
+++ onlineshop-war/src/de/java2enterprise/onlineshop/SellServlet.java
@@ -1,6 +1,10 @@
 package de.java2enterprise.onlineshop;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -23,7 +27,33 @@ public class SellServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		final String title = req.getParameter("title");
+		final String description = req.getParameter("description");
+		final String price = req.getParameter("price");
+
 		final Part part = req.getPart("foto");
+		OutputStream os = null;
+		InputStream is = null;
+
+		try {
+			String path = "/tmp/"
+					+ part.getSubmittedFileName();
+			File file = new File(path);
+			os = new FileOutputStream(file);
+			byte[] b = new byte[1024];
+			int i = 0;
+			is = part.getInputStream();
+			while ((i = is.read(b)) != -1) {
+				os.write(b, 0, i);
+			}
+		} catch (Exception ex) {
+			throw new ServletException(
+					ex.getMessage());
+		} finally {
+			os.close();
+			is.close();
+		}
+
 		final PrintWriter out = resp.getWriter();
 
 		out.println("<!DOCTYPE HTML>");
