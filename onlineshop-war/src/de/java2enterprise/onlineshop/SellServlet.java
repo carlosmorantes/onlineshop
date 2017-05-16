8b20597e4c6b746d2420c6a9134616a6919b55c8
carrcarl <carrcarl@DEL5484.ppal.directory>
2017-05-16 09:50:23 +0200

    set up multipart for uploading

--- onlineshop-war/src/de/java2enterprise/onlineshop/SellServlet.java
+++ onlineshop-war/src/de/java2enterprise/onlineshop/SellServlet.java
@@ -12,7 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
 @WebServlet("/sell")
-@MultipartConfig
+//max. size per file and total size
+//save in disk if the memory is full
+@MultipartConfig(
+		location="/tmp",
+		fileSizeThreshold=1024*1024,
+		maxFileSize=1024*1024*10, 
+		maxRequestSize=1024*1024*30)
 public class SellServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
