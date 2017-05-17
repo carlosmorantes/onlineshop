f98ff24427fee9adaf116b7b783de2f483d83836
carrcarl <carrcarl@DEL5484.ppal.directory>
2017-05-17 13:12:42 +0200

    add filter example

--- onlineshop-war/src/de/example/FilterExample.java
+++ onlineshop-war/src/de/example/FilterExample.java
@@ -5,30 +5,39 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
 
+@WebFilter(
+		urlPatterns={"/example"},
+		filterName="Example filter",
+		description="This is a example filter"
+		)
 public class FilterExample implements Filter {
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Destroy filter...");
+
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		
+		ServletContext sc = request.getServletContext();
+		sc.log("Forward request...");
+		chain.doFilter(request, response);
+
 	}
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Init filter...");
+
 	}
 
 
