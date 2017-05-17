cb3fef07a4ce64653ef2bb6314dcec4d3807b664
carrcarl <carrcarl@DEL5484.ppal.directory>
2017-05-17 13:14:57 +0200

    erase some imports

--- onlineshop-war/src/de/java2enterprise/onlineshop/SellServlet.java
+++ onlineshop-war/src/de/java2enterprise/onlineshop/SellServlet.java
@@ -1,12 +1,6 @@
 package de.java2enterprise.onlineshop;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
@@ -15,7 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 @WebServlet(
 		urlPatterns={"/sell"},
@@ -28,6 +21,11 @@ import javax.servlet.http.Part;
 		maxFileSize=1024*1024*10, 
 		maxRequestSize=1024*1024*30)
 public class SellServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {	
 		
